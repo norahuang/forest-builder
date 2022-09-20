bb6b29e05243c3e0cbc92d41079e12f94365b7cc
Huihui Nora Huang <huang.nora@microsoft.com>
2022-09-20 12:12:54 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-player.onChat("run", function () {
-	
-})
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace forestBuilder {
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
